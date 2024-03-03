742c80f82ba664c1d716978ba3693f2fc0ba8fd3
methylredd <interra361@gmail.com>
2024-03-03 21:59:16 +0800

    fix directory thing

--- Common/Directories.cs
+++ Common/Directories.cs
@@ -5,7 +5,7 @@ public static class Directories
 	public const string ModName = "StitchesLib";
 	public const string Mod = $"{ModName}/";
 
-	public const string Assets = $"{Mod}/Assets/";
+	public const string Assets = $"{Mod}Assets/";
 
 	public const string Textures = $"{Assets}Textures/";
 
