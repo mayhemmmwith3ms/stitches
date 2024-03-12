483b95d878c340fe4d17bb53915afbe07c9d1157
mayhemmm <interra361@gmail.com>
2024-03-12 18:40:10 +0800

    screentargets

---
+++ Common/Systems/ScreenTargets/ScreenTarget.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using Terraria;
+
+namespace StitchesLib.Common.Systems.ScreenTargets;
+public class ScreenTarget
+{
+	public ScreenTarget(Action _drawTo, Func<bool> _isVisible, Action<Vector2> _onResize = null, float _priority = 0)
+	{
+		drawTo = _drawTo;
+		isVisible = _isVisible;
+		onResize = _onResize;
+		Priority = _priority;
+
+		Vector2 dimensions = Utils.GraphicsUtils.ScreenRect.Size();
+
+		Main.QueueMainThreadAction(() =>
+		{
+			Target = new RenderTarget2D(Main.graphics.GraphicsDevice, (int)dimensions.X, (int)dimensions.Y, false, default, default, default, RenderTargetUsage.PreserveContents);
+		});
+
+		onResize?.Invoke(dimensions);
+
+		ScreenTargetManager.RegisterTarget(this);
+	}
+
+	public Action drawTo;
+
+	public Func<bool> isVisible;
+
+	public Action<Vector2> onResize;
+
+	public float Priority { get; set; } = 0;
+
+	public RenderTarget2D Target { get; set; }
+
+	public void ResizeTarget(Vector2 newSize)
+	{
+		Target?.Dispose();
+		Target = new RenderTarget2D(Main.graphics.GraphicsDevice, (int)newSize.X, (int)newSize.Y, false, default, default, default, RenderTargetUsage.PreserveContents);
+	}
+}
---
+++ Common/Systems/ScreenTargets/ScreenTargetManager.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace StitchesLib.Common.Systems.ScreenTargets;
+public class ScreenTargetManager : ILoadable
+{
+	public static List<ScreenTarget> Targets { get; private set; } = new();
+
+	public void Load(Mod mod)
+	{
+		On_Main.CheckMonoliths += On_Main_CheckMonoliths;
+		Main.OnResolutionChanged += Main_OnResolutionChanged;
+	}
+
+	public void Unload()
+	{
+		On_Main.CheckMonoliths -= On_Main_CheckMonoliths;
+		Main.OnResolutionChanged -= Main_OnResolutionChanged;
+
+		Main.QueueMainThreadAction(() =>
+		{
+			Targets.ForEach(t => t.Target?.Dispose());
+			Targets.Clear();
+			Targets = null;
+		});
+	}
+
+	public static void RegisterTarget(ScreenTarget target)
+	{
+		Targets.Add(target);
+		Targets.Sort((a, b) => a.Priority < b.Priority ? 1 : -1);
+	}
+
+	private void On_Main_CheckMonoliths(On_Main.orig_CheckMonoliths orig)
+	{
+		orig();
+
+		if (Main.dedServ)
+			return;
+
+		RenderTargetBinding[] bindingsCache = Main.graphics.GraphicsDevice.GetRenderTargets();
+
+		foreach(var tgt in Targets)
+		{
+			Main.spriteBatch.Begin();
+
+			Main.graphics.GraphicsDevice.SetRenderTarget(tgt.Target);
+			Main.graphics.GraphicsDevice.Clear(Color.Transparent);
+
+			if (tgt.isVisible())
+			{
+				tgt.drawTo();
+			}
+			
+			Main.spriteBatch.End();
+		}
+
+		Main.graphics.GraphicsDevice.SetRenderTargets(bindingsCache);
+	}
+
+	private void Main_OnResolutionChanged(Vector2 obj)
+	{
+		if (Main.dedServ)
+			return;
+
+		foreach (var tgt in Targets)
+		{
+			tgt.ResizeTarget(obj);
+		}
+	}
+}
